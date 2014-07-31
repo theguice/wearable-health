51e600bdd915d5a0d40e000218b1a4cd8d17eb45
Shubham Goel <shubham.goel@gmail.com>
2014-07-31 15:13:46 -0700

    compare tool can now add averages to the top bar

--- assets/_js/top_bars.js
+++ assets/_js/top_bars.js
@@ -29,15 +29,19 @@ var topBarRanges = {"heartrate":[0,200],
 					"gsr":[0,2],
 					"skin_temp":[0,200],
 					"air_temp":[0,200]};
+var bar_margin = {top: 0, right: 0, bottom: 30, left: 0},
+    bar_width = 100 - bar_margin.left - bar_margin.right,
+    bar_height = 120 - bar_margin.top - bar_margin.bottom;
+
+var bar_x = d3.scale.ordinal(),
+	bar_y = d3.scale.linear();
 
 for (i = 0; i < topBarSensors.length; i++)
 {
 	topBarData[topBarSensors[i]] = new Array();
 	initTopBar(topBarSensors[i]);
-/* 	top_bars(topBarSensors[i]); */
 }
 
-function top_bars(param) {
 
 
     /* 
@@ -57,60 +61,41 @@ function top_bars(param) {
     
     
     */
-	    
-}
+
 
 function addCompareRangeToTopBar()
 {
 	console.log('addCompareRangeToTopBar');
 	for (i = 0; i < topBarSensors.length; i++)
 	{
-	    var param = topBarSensors[i];
-		d3.json($base_url + "/api/parameter_averages.php?"+param+"=1&start_time="+startDateEpoc+"&end_time="+endDateEpoc, function(error, data)
+	    var sensor = topBarSensors[i];
+		d3.json($base_url + "/api/parameter_averages.php?"+sensor+"=1&start_time="+startDateEpoc+"&end_time="+endDateEpoc, function(error, data)
 		{
 			console.log(data[0].name+":"+data[0].value);
 		    topBarData[data[0].name].push(data[0].value);
-/* 		    animateAndUpdateTopbar(data[0].name); */
+		    animateAndUpdateTopbar(data[0].name);
 		});
 
 	}
 }
 
 
-var bar_margin = {top: 0, right: 0, bottom: 30, left: 0},
-    bar_width = 100 - bar_margin.left - bar_margin.right,
-    bar_height = 120 - bar_margin.top - bar_margin.bottom;
-
 function initTopBar(sensor)
 {
+	var data = topBarData[sensor];
+
     var svg_bar = d3.select("div."+sensor).append("svg")
 		.attr("width", 100)
 		.attr("height", 120)
 		.append("g")
 		.attr("class", sensor)
 		.attr("transform", "translate(" + 0 + "," + 0 + ")");
-        //TODO: update weight and height from parameters
-    
-    // start data dowload> which will update the charts automatically
-    d3.json($base_url + "/api/parameter_averages.php?"+sensor+"=1", function(error, data) {
-	    topBarData[data[0].name].push(data[0].value);
-	    // update the bar charts
-	    animateAndUpdateTopbar(data[0].name);
-	});
-
-}
-
-function animateAndUpdateTopbar(sensor)
-{
-
-	var data = topBarData[sensor];
-
-	var bar_x = d3.scale.ordinal()
-	    .rangeRoundBands([30, bar_width], .1)
+        //TODO: update width and height from parameters
+	
+	    bar_x.rangeRoundBands([30, bar_width], .2)
 	    .domain(d3.range(data.length));
 	
-	var bar_y = d3.scale.linear()
-	    .range([bar_height-5, 10])
+	    bar_y.range([bar_height-5, 10])
 	    .domain([topBarRanges[sensor][0], topBarRanges[sensor][1]]);
 	
     var xAxis = d3.svg.axis()
@@ -122,8 +107,6 @@ function animateAndUpdateTopbar(sensor)
         .orient("left")
         .ticks(1);
 
-	var svg_bar = d3.select("g."+sensor);
-	
     svg_bar.append("g")
 		.attr("class", "y axis")
 		.attr("transform", "translate(30,20)")
@@ -141,34 +124,38 @@ function animateAndUpdateTopbar(sensor)
 		.attr('x', '30')
 		.attr('y', '0')
 		.html(function(d,i) { return "<label class='checkbox'><input type='checkbox' id='option_" + sensor + "' ></input>" + sensor + "</label>"; });
+    
+    // start data dowload> which will update the charts automatically
+    d3.json($base_url + "/api/parameter_averages.php?"+sensor+"=1", function(error, data) {
+	    topBarData[data[0].name].push(data[0].value);
+	    // update the bar charts
+	    animateAndUpdateTopbar(data[0].name);
+	});
 
-	svg_bar.selectAll(".bar")
-		.data(data)
-		.enter().append("rect")
-		.attr("class", "bar")
-		.attr("x", function(d,i) { return bar_x(i); })
-		.attr("width", bar_x.rangeBand())
-		.attr("y", function(d) { return bar_y(d); })
-		.attr("height", function(d) { return bar_height - bar_y(d) + 15; });
-/*
-	var bars = svg_bar.selectAll(".bar")
-				.data(data);
-	
-    bars.enter()
-        .append("rect")
-        .attr("class", "bar");
-*/
-
-/*
-	bars
-		.transition()
-		.duration(300)
-		.ease("quad")
-		    .attr("width", x.rangeBand())
-		    .attr("height", function(d) { return height - bar_y(d.value) + 15; })
-		    .attr("transform", function(d,i) {
-		        return "translate(" + [bar_x(i), 0] + ")"
-		    });
-*/
 }
 
+
+function animateAndUpdateTopbar(sensor)
+{
+
+	var data = topBarData[sensor];
+	
+	bar_x.domain(d3.range(data.length));
+
+	var svg_bar = d3.select("g."+sensor);
+	
+	var bars = svg_bar.selectAll("rect.bar")
+		.data(data);
+		
+	bars.enter()
+		.append("rect")
+		.attr("class", "bar");
+		
+	bars.transition()
+	    .duration(300)
+	    .ease("quad")
+		.attr("width", bar_x.rangeBand())
+		.attr("height", function(d) { return bar_height - bar_y(d) + 15; })
+		.attr("y", function(d) { return bar_y(d); })
+		.attr("x", function(d,i) { return bar_x(i); });
+}
