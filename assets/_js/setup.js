5549f5eb2a16d682919aed7d752e8d3e11a7873a
Shubham Goel <shubham.goel@gmail.com>
2014-08-07 13:44:35 -0700

    base line hidden when time series line is hidden

--- assets/_js/setup.js
+++ assets/_js/setup.js
@@ -11,8 +11,10 @@ if (!$("#option_heartrate").is(":checked")) {
 $(".top").on('click', function() {
 	if ($('#option_heartrate').is(":checked")) {
 		$(".line.line0").css("visibility", "visible");
+		$(".line.base0").css("visibility", "visible");
 	} else {
 		$(".line.line0").css("visibility", "hidden");
+		$(".line.base0").css("visibility", "hidden");
 	}
 	if ($('#option_steps').is(":checked")) {
 		$(".bar.bar1").css("visibility", "visible");
@@ -26,22 +28,38 @@ $(".top").on('click', function() {
 	}
 	if ($('#option_gsr').is(":checked")) {
 		$(".line.line3").css("visibility", "visible");
+		$(".line.base3").css("visibility", "visible");
 	} else {
 		$(".line.line3").css("visibility", "hidden");
+		$(".line.base3").css("visibility", "hidden");
 	}
 	if ($('#option_posture').is(":checked")) {
 		$(".line.line4").css("visibility", "visible");
+		$(".line.base4").css("visibility", "visible");
 	} else {
 		$(".line.line4").css("visibility", "hidden");
+		$(".line.base4").css("visibility", "hidden");
 	}
 	if ($('#option_skin_temp').is(":checked")) {
 		$(".line.line5").css("visibility", "visible");
+		$(".line.base5").css("visibility", "visible");
 	} else {
 		$(".line.line5").css("visibility", "hidden");
+		$(".line.base5").css("visibility", "hidden");
 	}
 	if ($('#option_air_temp').is(":checked")) {
 		$(".line.line6").css("visibility", "visible");
+		$(".line.base6").css("visibility", "visible");
 	} else {
 		$(".line.line6").css("visibility", "hidden");
+		$(".line.base6").css("visibility", "hidden");
 	}
+//	if ($('#option_posture').is(":checked")) {
+//		$(".line.line7").css("visibility", "visible");
+//		$(".line.base7").css("visibility", "visible");
+//	} else {
+//		$(".line.line7").css("visibility", "hidden");
+//		$(".line.base7").css("visibility", "hidden");
+//	}
+
 });
