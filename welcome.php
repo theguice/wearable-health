6edd3df521c476a3105391768266b36b878e7436
Shubham Goel <shubham.goel@gmail.com>
2014-08-13 11:32:29 -0700

    welcome page updated

--- welcome.php
+++ welcome.php
@@ -19,42 +19,56 @@ if(empty($_SESSION['user']))
 // We can display the user's username to them by reading it from the session array.  Remember that because 
 // a username is user submitted content we must use htmlentities on it before displaying it to the user. 
 ?> 
-<script type="text/javascript" src="assets/_lib/jquery-1.11.1.min.js"></script>
-<link rel="stylesheet" href="assets/_lib/bootstrap/css/bootstrap.min.css">
-<script src="assets/_lib/bootstrap/js/bootstrap.min.js"></script>
-<link rel="stylesheet" href="assets/_css/basic.css">
 
-  <div class="center">
-    <h1>Welcome, <?php echo htmlentities($_SESSION['user']['username'], ENT_QUOTES, 'UTF-8'); ?></h1><br />
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>Welcome</title>
+  <link rel='stylesheet' href='http://codepen.io/assets/libs/fullpage/jquery-ui.css'>
+  <link rel="stylesheet" href="assets/_css/login.css" type="text/css">
+</head>
 
-    <div class="checks">
-      <h3>Before we begin:</h3>
-      <div id="moves">
-	<label><input type="checkbox" class="checks required"> I have imported my latest moves data</label><br/>
-	<ul>
-	  <li><a href="https://accounts.moves-app.com/export">Download your latest moves data here</a></li>
-	  <li>Upload your moves-export zip file here:</li>
-<form action="upload.php" method="post" enctype="multipart/form-data">
-<label for="file">Filename:</label>
-<input type="file" name="file" id="file"><br>
-<input type="submit" name="submit" value="Submit">
-</form>
+<body>
+  <div class="login-card instructions">
+    <h1>Welcome <?php echo ucfirst($_SESSION['user']['username']); ?></h1><br>
+    <h3>Before we begin:</h3>
+    
+<div id="moves">
+<h3><input type="checkbox" class="checks required">Moves</h3>
+    <label>I have imported my latest moves data</label>
+    <ul>
+		<li><a href="https://accounts.moves-app.com/export">Download your latest moves data here</a></li>
+		<li>Upload your moves-export zip file here:</li>
 	</ul>
-      </div>
-      <div id="basis">
-	<label><input type="checkbox" class="checks required"> I have plugged in my basis watch to charge & sync</label><br/>
-        <img width="150" height="auto" src="assets/_img/basis-charging.jpg"/>
-      </div>
-      <div id="lumo">
-	<label><input type="checkbox" class="checks required"> I have launched the Lumo App on my smartphone to sync</label><br/>
-        <img width="150" height="auto" src="assets/_img/lumo-sync.jpg"/>
-      </div>
-    </div>
-      <div id="button" style="display:none;" class="center">
-        <a href="dashboard.php"><button type="button" class="btn btn-lg btn-success">View Data Portal</button></a>
-      </div>
-  </div>
+	<form action="upload.php" method="post" enctype="multipart/form-data">
+	    <label for="file">Filename:</label>
+	    <input type="file" name="file" id="file"><br>
+	    <input type="submit" name="submit" value="Upload" class="login login-submit">
+	</form>
+</div>
+<div id="basis">
+<h3><input type="checkbox" class="checks required">Basis</h3>
 
+	<label>I have plugged in my basis watch to charge & sync</label><br/>
+	<img width="150" height="auto" src="assets/_img/basis-charging.jpg"/>
+</div>
+
+<div id="lumo">
+<h3><input type="checkbox" class="checks required">Lumo</h3>
+
+	<label>I have launched the Lumo App on my smartphone to sync</label><br/>
+	<img width="150" height="auto" src="assets/_img/lumo-sync.jpg"/>
+</div>
+
+<div class="login login-submit" id="button" style="display:none;">
+  <a class="login login-submit" href="dashboard.php"><button type="button" class="login login-submit">View Data Portal</button></a>
+</div>
+
+
+   
+</div>
+  <script src='http://codepen.io/assets/libs/fullpage/jquery_and_jqueryui.js'></script>
 <script>
   $(".required").on('change', function(){
       console.log($('.required:checked').length, $('.required').length);
@@ -65,3 +79,7 @@ if(empty($_SESSION['user']))
     }
   });
 </script>
+
+</body>
+
+</html>
