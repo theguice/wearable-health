c3dd11bb9a06a992b4946509d3c3248cc3f2524f
Shubham Goel <shubham.goel@gmail.com>
2014-08-14 16:17:40 -0700

    unsecured urls fixed top bar icons fixed

--- dashboard.php
+++ dashboard.php
@@ -155,7 +155,7 @@ $userid = ucfirst($_SESSION['user']['u_id']);
     <script src="assets/_js/top_bars.js" type="text/javascript"></script>
     <!-- map_custom.js is where you specify map style, and setup markers -->
     <script src="assets/_js/map_custom.js" type="text/javascript"></script>
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
     <script>window.jQuery || document.write('<script src="assets/_js/vendor/jquery-1.8.0.min.js"><\/script>')</script>
 
     <script src="assets/_js/plugins.js"></script>
